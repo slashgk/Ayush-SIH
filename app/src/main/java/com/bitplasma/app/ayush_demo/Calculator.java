105d8ad411888e6d85127dd2ea8e593979cd428a
GAURAV KUMAR <gaurav.grd@gmail.com>
2018-03-30 17:59:28 +0530

    calculator done

--- app/src/main/java/com/bitplasma/app/ayush_demo/Calculator.java
+++ app/src/main/java/com/bitplasma/app/ayush_demo/Calculator.java
@@ -2,6 +2,7 @@ package com.bitplasma.app.ayush_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,6 +17,7 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 
 public class Calculator extends AppCompatActivity {
@@ -29,6 +31,7 @@ public class Calculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        Log.e(LOG_TAG,Integer.toString(UnitValues.time_units.length)+" "+Integer.toString(UnitValues.time_values.length));
         rg=(RadioGroup)findViewById(R.id.RGroup);
         sp1=(Spinner)findViewById(R.id.sp);
         sp2=(Spinner)findViewById(R.id.output_sp);
@@ -42,6 +45,36 @@ public class Calculator extends AppCompatActivity {
         sp1.setAdapter(adapter);
         sp2.setAdapter(adapter);
         output=(TextView)findViewById(R.id.output_tv);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton radioButton = (RadioButton) findViewById(i);
+                if(radioButton.getText().toString().equals("Length")){
+                    String arraySpinner[] = getResources().getStringArray(R.array.length_array);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(Calculator.this,
+                            android.R.layout.simple_spinner_item, arraySpinner);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    sp1.setAdapter(adapter);
+                    sp2.setAdapter(adapter);
+                }
+                if(radioButton.getText().toString().equals("Time")){
+                    String arraySpinner[] = getResources().getStringArray(R.array.time_array);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(Calculator.this,
+                            android.R.layout.simple_spinner_item, arraySpinner);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    sp1.setAdapter(adapter);
+                    sp2.setAdapter(adapter);
+                }
+                if(radioButton.getText().toString().equals("Weight")){
+                    String arraySpinner[] = getResources().getStringArray(R.array.weight_array);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(Calculator.this,
+                            android.R.layout.simple_spinner_item, arraySpinner);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    sp1.setAdapter(adapter);
+                    sp2.setAdapter(adapter);
+                }
+            }
+        });
         converter.setOnClickListener(new Button.OnClickListener()
         {
 
@@ -70,7 +103,43 @@ public class Calculator extends AppCompatActivity {
                         }
                     }
                     double out_unitss = seconds/UnitValues.time_values[i];
-                    output.setText(Double.valueOf(out_unitss).toString());
+                    output.setText(BigDecimal.valueOf(out_unitss).toPlainString());
+                }
+                else  if(rad.equals("Length")){
+                    int i=0;
+                    for(i=0;i<UnitValues.length_units.length;i++){
+                        if(inp_unit.equals(UnitValues.length_units[i])){
+                            break;
+                        }
+                    }
+                    Log.e(LOG_TAG,Integer.valueOf(i).toString());
+                    double seconds = input*UnitValues.length_values[i];
+                    Log.e(LOG_TAG,"3");
+                    for(i=0;i<UnitValues.time_units.length;i++){
+                        if(out_unit.equals(UnitValues.length_units[i])){
+                            break;
+                        }
+                    }
+                    double out_unitss = seconds/UnitValues.length_values[i];
+                    output.setText(BigDecimal.valueOf(out_unitss).toPlainString());
+                }
+                else  if(rad.equals("Weight")){
+                    int i=0;
+                    for(i=0;i<UnitValues.weight_units.length;i++){
+                        if(inp_unit.equals(UnitValues.weight_units[i])){
+                            break;
+                        }
+                    }
+                    Log.e(LOG_TAG,Integer.valueOf(i).toString());
+                    double seconds = input*UnitValues.weight_values[i];
+                    Log.e(LOG_TAG,"3");
+                    for(i=0;i<UnitValues.time_units.length;i++){
+                        if(out_unit.equals(UnitValues.weight_units[i])){
+                            break;
+                        }
+                    }
+                    double out_unitss = seconds/UnitValues.weight_values[i];
+                    output.setText(BigDecimal.valueOf(out_unitss).toPlainString());
                 }
             }
         });
--- app/src/main/java/com/bitplasma/app/ayush_demo/UnitValues.java
+++ app/src/main/java/com/bitplasma/app/ayush_demo/UnitValues.java
@@ -7,7 +7,10 @@ import java.util.HashMap;
  */
 
 public class UnitValues {
-    static String time_units[]={"KSANA","LAVA","NIMESHA","KASTHA","KALA","GHATI","MUHURTA","AHORATRA","PAKSA","MASA","MASA","RITU","AYANA","SAMVATSARA","YUGA","SECOND","MINUTES","HOUR","DAY","MONTHS","YEAR"};
-    static double time_values[]={0.38,0.77,1.55,4.66,140.0,1440.0,2880.0,86400.0,1.296e+6,2.592e+6,5.184e+6,1.577e+7,3.154e+7,1.577e+8,1,};
-
+    static String time_units[]={"KSANA","LAVA","NIMESHA","KASTHA","KALA","GHATI","MUHURTA","AHORATRA","PAKSA","MASA","RITU","AYANA","SAMVATSARA","YUGA","SECOND","MINUTES","HOUR","DAY","MONTH","YEAR"};
+    static double time_values[]={0.38,0.77,1.55,4.66,140.0,1440.0,2880.0,86400.0,1.296e+6,2.592e+6,5.184e+6,1.577e+7,3.154e+7,1.577e+8,1,60,3600,86400,2.628e+6,3.154e+7};
+    static String length_units[]={"YAVODARA","ANGULA","BITAHASTI","ARATNI","HASTA","RAJAHASTA(NRIPAHASTA)","VYAMA","CENTIMETER","METER"};
+    static double length_values[]={0.24,1.95,22.86,41.91,45.72,55.88,182.88,1,100};
+    static String weight_units[]={"PARAMANU","DHAVANSHI(VANSI and TRASRENU)","MARICHI","LAL SARSHAP","TUNDAL","DHANYAMASH","YAVA","RATTI","ANDIKA","MASHAK (MASA)","SHAAN","KOL","TOLA","MILIGRAM","GRAM","KILOGRAM"};
+    static double weight_values[]={0.0016,0.05,0.32,1.95,15.62,31.25,62.5,125,250,1000,3000,6000,12000,1,1000,1000000};
 }
